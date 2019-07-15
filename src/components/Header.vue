23d1069cb8a97f7e3faffade4f0e188a7bd62974
Vadim Lavorchuk <lavorchuk.vadim@digitalsuits.co>
2019-07-15 14:17:19 +0300

    - Added admin sidebar template - Add fa library - Add admin page routing link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,6 +20,9 @@
 
       </ul>
       <ul class="nav navbar-nav">
+        <li v-if="!isLoggedIn" class="li-pointer nav-item">
+          <router-link class="nav-link" to="/admin">Admin panel</router-link>
+        </li>
         <router-link to="/login" tag="li" v-if="!isLoggedIn" class="nav-item" active-class="active">
           <a class="nav-link">Login</a>
         </router-link>
@@ -29,7 +32,7 @@
         <router-link to="/register" tag="li" v-if="!isLoggedIn" class="nav-item" active-class="active">
           <a class="nav-link">Register</a>
         </router-link>
-        <li>
+        <li v-if="isLoggedIn">
           <router-link to="/cart" class="btn btn-success navbar-btn" tag="button">
             Checkout <span class="badge badge-light">{{ numItems }} ($ {{ cartValue }})</span>
           </router-link>
